0b230a27627831da86239f3fc90ca9588ffc23f1
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-08-06 16:05:05 +0300

    first commit

---
+++ 10.pl
@@ -0,0 +1,18 @@
+
+my $max_prime = 2_000_000;
+my @primes = (0..$max_prime-1);
+$primes[1]=0;
+for (my $i=2; $i<$max_prime/2; ) {
+	if ($primes[$i]) {
+		for (my $j=$i*2; $j<=$max_prime; $j+=$i) {
+			$primes[$j]=0;
+		}
+	}
+	$i++;
+}
+@primes = grep { $_ } @primes;
+
+my $sum = 0;
+$sum += $_ for @primes;
+
+print "$sum\n";
---
+++ 7.pl
@@ -0,0 +1,16 @@
+
+my $max_prime = 1000000;
+my @primes = (0..$max_prime);
+$primes[1]=0;
+for (my $i=2; $i<$max_prime/2; ) {
+	if ($primes[$i]) {
+		for (my $j=$i*2; $j<=$max_prime; $j+=$i) {
+			$primes[$j]=0;
+		}
+	}
+	$i++;
+}
+@primes = grep { $_ } @primes;
+
+print "$primes[10000]\n";
+
